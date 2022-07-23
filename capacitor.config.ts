9b82d61664696da341a32875852a6730863614c7
P-Alcantara <jamespiolo.nocillado@g.batstate-u.edu.ph>
2022-07-23 18:47:16 +0800

    Kick starter

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionx',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
